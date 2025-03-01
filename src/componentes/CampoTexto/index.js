698247587f6e47e3f5914d52d2224c25b2694a72
VickyPrado <vicky.prado05@gmail.com>
2025-03-01 17:36:37 -0300

    manipulando-input-controlando-estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,16 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` 
 
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
