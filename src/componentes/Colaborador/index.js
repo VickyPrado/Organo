e2702ddff67d8b24d39610376bcace6ea8f2847c
VickyPrado <vicky.prado05@gmail.com>
2025-03-10 14:47:12 -0300

    add-card-de-colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://i.pinimg.com/550x/1d/d9/0b/1dd90beab86e84f316c72940244b7bdb.jpg' alt='Pernalonga' />
+            </div>
+            <div className='rodape'>
+                <h4>Pernalonga</h4>
+                <h5>Instrutor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
