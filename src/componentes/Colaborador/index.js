ada42b683318a10676fc52aff691a2c1135aa5f2
VickyPrado <vicky.prado05@gmail.com>
2025-03-10 21:07:56 -0300

    map-colaboradores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://i.pinimg.com/550x/1d/d9/0b/1dd90beab86e84f316c72940244b7bdb.jpg' alt='Pernalonga' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Pernalonga</h4>
-                <h5>Instrutor</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
